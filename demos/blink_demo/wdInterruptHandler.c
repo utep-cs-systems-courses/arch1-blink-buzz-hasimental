c5060f9639b3a3d93b1343d5ac5c9bafdfc637b4
student <hasimental@miners.utep.edu>
2021-04-01 21:12:32 -0600

    messed around with the interrupt handler in the blink demo and messed around with the other demos

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -4,7 +4,7 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  if (++blink_count == 20) { // changed to 20 interrupts/sec
     state_advance();
     blink_count = 0;
   }
